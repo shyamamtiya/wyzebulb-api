c3e189096bcb04e65cd05849ae2fb75ee40b7161
mvmshyam@gmail.com <mvmshyam@gmail.com>
2018-09-04 14:26:11 +0530

    initial

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	res.render('index', { title: 'Wyzebulb' });
+});
+router.post('/', function(req, res, next) {
+	res.render('filter', {
+		mobile: req.body.mobile || 1234567890,
+		carttotal: req.body.carttotal || 0
+	});
+});
+router.post('/trigger', function(req, res, next) {
+	res.render('trigger', {
+		carttotal: req.body.carttotal,
+		operator: req.body.sel2,
+		inpValue: req.body.filtervalue,
+		mobile: req.body.mobile
+	});
+	console.log('body', req.body);
+});
+module.exports = router;
---
+++ routes/sms.js
@@ -0,0 +1,93 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+var jsonObj = {
+	toNumber: '7031949003',
+	smsText: 'Food items are waiting in your cart. Continue checkout to place your order.'
+};
+var options = {
+	url: 'https://leado-mini-project-api.herokuapp.com/sendSms',
+	headers: {
+		'Content-Type': 'application/json',
+		'x-access-token':
+			'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiU2h5YW0ifQ.a9R1PRhBuYObDt3nSrkqO58emrv4-HLbVKqfz56tf5U'
+	},
+	body: jsonObj,
+	json: true
+};
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	res.sendStatus(200);
+});
+
+router.post('/sms', function(req, res, next) {
+	console.log(req.body);
+	let s = req.body.operator;
+	jsonObj.smsText = req.body.msg;
+	jsonObj.toNumber = req.body.mobile;
+	switch (s) {
+		case 'lesser than': {
+			if (req.body.carttotal < req.body.value) {
+				sendSMS(req, res, function(obj) {
+					res.send(obj);
+				});
+			} else {
+				res.send('Unable to send msg due to condition mismatch');
+			}
+			break;
+		}
+		case 'greater than': {
+			if (req.body.carttotal > req.body.value) {
+				sendSMS(req, res, function(obj) {
+					res.send(obj);
+				});
+			} else {
+				res.send('Unable to send msg due to condition mismatch');
+			}
+			break;
+		}
+		case 'equals': {
+			if (req.body.carttotal == req.body.value) {
+				sendSMS(req, res, function(obj) {
+					res.send(obj);
+				});
+			} else {
+				res.send('Unable to send msg due to condition mismatch');
+			}
+			break;
+		}
+		case 'not equals': {
+			if (req.body.carttotal != req.body.value) {
+				sendSMS(req, res, function(obj) {
+					res.send(obj);
+				});
+			}
+			break;
+		}
+		case 'contains': {
+			if (req.body.carttotal > 0) {
+				sendSMS(req, res, function(obj) {
+					res.send(obj);
+				});
+			} else {
+				res.send('Unable to send msg due to condition mismatch');
+			}
+			break;
+		}
+	}
+});
+
+function sendSMS(req, response, done) {
+	request.post(options, function(err, res) {
+		console.log('indside cld');
+		if (err) {
+			console.log('err', err);
+			throw err;
+		}
+		console.log(res.body);
+		done(res.body);
+	});
+}
+
+module.exports = router;
